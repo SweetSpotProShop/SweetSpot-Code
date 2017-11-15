dea7e2dbd4e60715b426100644371c22998c9b20
TGills <tylerguillaume@live.com>
2017-11-15 08:59:33 -0600

    Sales Report Export
    Should be fully functioning now

--- SweetSpotDiscountGolfPOS/ReportsSales.aspx.cs
+++ SweetSpotDiscountGolfPOS/ReportsSales.aspx.cs
@@ -1,9 +1,11 @@
-﻿using SweetShop;
+﻿using OfficeOpenXml;
+using SweetShop;
 using SweetSpotDiscountGolfPOS.ClassLibrary;
 using SweetSpotProShop;
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.IO;
 using System.Linq;
 using System.Threading;
 using System.Web;
@@ -25,6 +27,8 @@ namespace SweetSpotDiscountGolfPOS
         DateTime endDate;
         double salesDollars;
 
+        DataTable dt = new DataTable();
+
         protected void Page_Load(object sender, EventArgs e)
         {
             //Collects current method and page for error tracking
@@ -39,27 +43,24 @@ namespace SweetSpotDiscountGolfPOS
                     //Go back to Login to log in
                     Server.Transfer("LoginPage.aspx", false);
                 }
-                if (!IsPostBack)
+                //Gathering the start and end dates
+                Object[] passing = (Object[])Session["reportInfo"];
+                DateTime[] reportDates = (DateTime[])passing[0];
+                DateTime startDate = reportDates[0];
+                DateTime endDate = reportDates[1];
+                int locationID = (int)passing[1];
+                //Builds string to display in label
+                if (startDate == endDate)
                 {
-                    //Gathering the start and end dates
-                    Object[] passing = (Object[])Session["reportInfo"];
-                    DateTime[] reportDates = (DateTime[])passing[0];
-                    DateTime startDate = reportDates[0];
-                    DateTime endDate = reportDates[1];
-                    int locationID = (int)passing[1];
-                    //Builds string to display in label
-                    if (startDate == endDate)
-                    {
-                        lblDates.Text = "Items sold on: " + startDate.ToString("d") + " for " + l.locationName(locationID);
-                    }
-                    else
-                    {
-                        lblDates.Text = "Items sold on: " + startDate.ToString("d") + " to " + endDate.ToString("d") + " for " + l.locationName(locationID);
-                    }
-                    DataTable dt = r.returnSalesForSelectedDate(passing);
-                    grdSalesByDate.DataSource = dt;
-                    grdSalesByDate.DataBind();
+                    lblDates.Text = "Items sold on: " + startDate.ToString("d") + " for " + l.locationName(locationID);
                 }
+                else
+                {
+                    lblDates.Text = "Items sold on: " + startDate.ToString("d") + " to " + endDate.ToString("d") + " for " + l.locationName(locationID);
+                }
+                dt = r.returnSalesForSelectedDate(passing);
+                grdSalesByDate.DataSource = dt;
+                grdSalesByDate.DataBind();
             }
             //Exception catch
             catch (ThreadAbortException tae) { }
@@ -84,6 +85,60 @@ namespace SweetSpotDiscountGolfPOS
             else if (e.Row.RowType == DataControlRowType.Footer)
             {
                 e.Row.Cells[1].Text = String.Format("{0:C}", salesDollars);
+            }
+        }
+        protected void btnDownload_Click(object sender, EventArgs e)
+        {
+            //Collects current method for error tracking
+            string method = "btnDownload_Click";
+            try
+            {
+                //Sets path and file name to download report to
+                string pathUser = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+                string pathDownload = (pathUser + "\\Downloads\\");
+                Object[] passing = (Object[])Session["reportInfo"];
+                string loc = l.locationName(Convert.ToInt32(passing[1]));
+                string fileName = "Sales Report by Date - " + loc + ".xlsx";
+                FileInfo newFile = new FileInfo(pathDownload + fileName);
+                using (ExcelPackage xlPackage = new ExcelPackage(newFile))
+                {
+                    //Creates a seperate sheet for each data table
+                    ExcelWorksheet purchasesExport = xlPackage.Workbook.Worksheets.Add("Purchases");
+                    // write to sheet   
+                    purchasesExport.Cells[1, 1].Value = lblDates.Text;
+                    purchasesExport.Cells[2, 1].Value = "Date";
+                    purchasesExport.Cells[2, 2].Value = "Sales Dollars";
+                    int recordIndex = 3;
+                    foreach (DataRow row in dt.Rows)
+                    {
+                        DateTime d = (DateTime)row[0];
+                        purchasesExport.Cells[recordIndex, 1].Value = d.ToString("d");
+                        purchasesExport.Cells[recordIndex, 2].Value = row[1].ToString();
+                        recordIndex++;
+                    }
+                    Response.Clear();
+                    Response.AddHeader("content-disposition", "attachment; filename=\"" + fileName + "\"");
+                    Response.ContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+                    Response.BinaryWrite(xlPackage.GetAsByteArray());
+                    Response.End();
+                }
+            }
+            //Exception catch
+            catch (ThreadAbortException tae) { }
+            catch (Exception ex)
+            {
+                //Log employee number
+                int employeeID = cu.empID;
+                //Log current page
+                string currPage = Convert.ToString(Session["currPage"]);
+                //Log all info into error table
+                er.logError(ex, employeeID, currPage, method, this);
+                //string prevPage = Convert.ToString(Session["prevPage"]);
+                //Display message box
+                MessageBox.ShowMessage("An Error has occured and been logged. "
+                    + "If you continue to receive this message please contact "
+                    + "your system administrator", this);
+                //Server.Transfer(prevPage, false);
             }
         }
     }
