ccff833d405c1bf7b010d535349dc331d27b3f4b
TGills <tylerguillaume@live.com>
2017-11-15 09:21:50 -0600

    Payment Type Report Export

--- SweetSpotDiscountGolfPOS/ReportsPaymentType.aspx.cs
+++ SweetSpotDiscountGolfPOS/ReportsPaymentType.aspx.cs
@@ -1,9 +1,11 @@
-﻿using SweetShop;
+﻿using OfficeOpenXml;
+using SweetShop;
 using SweetSpotDiscountGolfPOS.ClassLibrary;
 using SweetSpotProShop;
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.IO;
 using System.Linq;
 using System.Threading;
 using System.Web;
@@ -29,6 +31,8 @@ namespace SweetSpotDiscountGolfPOS
         double salesMastercard;
         double salesVisa;
 
+        DataTable dt = new DataTable();
+
         protected void Page_Load(object sender, EventArgs e)
         {
             //Collects current method and page for error tracking
@@ -43,27 +47,26 @@ namespace SweetSpotDiscountGolfPOS
                     //Go back to Login to log in
                     Server.Transfer("LoginPage.aspx", false);
                 }
-                if (!IsPostBack)
+
+                //Gathering the start and end dates
+                Object[] passing = (Object[])Session["reportInfo"];
+                DateTime[] reportDates = (DateTime[])passing[0];
+                DateTime startDate = reportDates[0];
+                DateTime endDate = reportDates[1];
+                int locationID = (int)passing[1];
+                //Builds string to display in label
+                if (startDate == endDate)
                 {
-                    //Gathering the start and end dates
-                    Object[] passing = (Object[])Session["reportInfo"];
-                    DateTime[] reportDates = (DateTime[])passing[0];
-                    DateTime startDate = reportDates[0];
-                    DateTime endDate = reportDates[1];
-                    int locationID = (int)passing[1];
-                    //Builds string to display in label
-                    if (startDate == endDate)
-                    {
-                        lblDates.Text = "Items sold on: " + startDate.ToString("d") + " for " + l.locationName(locationID);
-                    }
-                    else
-                    {
-                        lblDates.Text = "Items sold on: " + startDate.ToString("d") + " to " + endDate.ToString("d") + " for " + l.locationName(locationID);
-                    }
-                    DataTable dt = r.returnSalesByPaymentTypeForSelectedDate(passing);
-                    grdSalesByDate.DataSource = dt;
-                    grdSalesByDate.DataBind();
+                    lblDates.Text = "Items sold on: " + startDate.ToString("d") + " for " + l.locationName(locationID);
                 }
+                else
+                {
+                    lblDates.Text = "Items sold on: " + startDate.ToString("d") + " to " + endDate.ToString("d") + " for " + l.locationName(locationID);
+                }
+                dt = r.returnSalesByPaymentTypeForSelectedDate(passing);
+                grdSalesByDate.DataSource = dt;
+                grdSalesByDate.DataBind();
+
             }
             //Exception catch
             catch (ThreadAbortException tae) { }
@@ -96,6 +99,68 @@ namespace SweetSpotDiscountGolfPOS
                 e.Row.Cells[3].Text = String.Format("{0:C}", salesGiftCard);
                 e.Row.Cells[4].Text = String.Format("{0:C}", salesMastercard);
                 e.Row.Cells[5].Text = String.Format("{0:C}", salesVisa);
+            }
+        }
+        protected void btnDownload_Click(object sender, EventArgs e)
+        {
+            //Collects current method for error tracking
+            string method = "btnDownload_Click";
+            try
+            {
+                //Sets path and file name to download report to
+                string pathUser = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+                string pathDownload = (pathUser + "\\Downloads\\");
+                Object[] passing = (Object[])Session["reportInfo"];
+                string loc = l.locationName(Convert.ToInt32(passing[1]));
+                string fileName = "Payment Type Report - " + loc + ".xlsx";
+                FileInfo newFile = new FileInfo(pathDownload + fileName);
+                using (ExcelPackage xlPackage = new ExcelPackage(newFile))
+                {
+                    //Creates a seperate sheet for each data table
+                    ExcelWorksheet paymentTypeExport = xlPackage.Workbook.Worksheets.Add("Payment Type");
+                    // write to sheet   
+                    paymentTypeExport.Cells[1, 1].Value = lblDates.Text;
+                    paymentTypeExport.Cells[2, 1].Value = "Date";
+                    paymentTypeExport.Cells[2, 2].Value = "Cash";
+                    paymentTypeExport.Cells[2, 3].Value = "Debit";
+                    paymentTypeExport.Cells[2, 4].Value = "Gift Card";
+                    paymentTypeExport.Cells[2, 5].Value = "Mastercard";
+                    paymentTypeExport.Cells[2, 6].Value = "Visa";
+                    int recordIndex = 3;
+                    foreach (DataRow row in dt.Rows)
+                    {
+                        DateTime d = (DateTime)row[0];
+                        paymentTypeExport.Cells[recordIndex, 1].Value = d.ToString("d");
+                        paymentTypeExport.Cells[recordIndex, 2].Value = row[1];
+                        paymentTypeExport.Cells[recordIndex, 3].Value = row[2];
+                        paymentTypeExport.Cells[recordIndex, 4].Value = row[3];
+                        paymentTypeExport.Cells[recordIndex, 5].Value = row[4];
+                        paymentTypeExport.Cells[recordIndex, 6].Value = row[5];
+                        recordIndex++;
+                    }
+                    Response.Clear();
+                    Response.AddHeader("content-disposition", "attachment; filename=\"" + fileName + "\"");
+                    Response.ContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+                    Response.BinaryWrite(xlPackage.GetAsByteArray());
+                    Response.End();
+                }
+            }
+            //Exception catch
+            catch (ThreadAbortException tae) { }
+            catch (Exception ex)
+            {
+                //Log employee number
+                int employeeID = cu.empID;
+                //Log current page
+                string currPage = Convert.ToString(Session["currPage"]);
+                //Log all info into error table
+                er.logError(ex, employeeID, currPage, method, this);
+                //string prevPage = Convert.ToString(Session["prevPage"]);
+                //Display message box
+                MessageBox.ShowMessage("An Error has occured and been logged. "
+                    + "If you continue to receive this message please contact "
+                    + "your system administrator", this);
+                //Server.Transfer(prevPage, false);
             }
         }
     }
