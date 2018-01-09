113a47bf93b3996f36f827bddc881ae336faac3f
TGills <tylerguillaume@live.com>
2018-01-09 13:37:26 -0600

    Included updated item import and new class
    -The cleaned item import method has been added but not linked to the import lifecycle.
    -Added a new class "CustomExtensions" to work alongside the new item import method.

---
+++ SweetSpotDiscountGolfPOS/ClassLibrary/CustomExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SweetSpotDiscountGolfPOS.ClassLibrary
+{
+    public static class CustomExtensions
+    {
+        public static string ToNullSafeString(this object obj)
+        {
+            return (obj ?? string.Empty).ToString();
+        }
+
+
+    }
+}
